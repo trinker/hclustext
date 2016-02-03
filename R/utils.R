17776e1068bebb9443282b56270dc53027defb7b
Tyler <tyler.rinker@gmail.com>
2016-02-02 22:14:10 -0500

    added funs to utils

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,10 @@ pn <- function(x, y) {
     m <- prettyNum(x, big.mark = ",", scientific = FALSE)
     paste0(paste(rep(" ", y - nchar(m)), collapse = ""), m)
 }
+
+
+# min max scaling function
+min_max <- function(x) (x - min(x, na.rm = TRUE))/(max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
+
+
+above <- function(x, threshhold) which(x >= threshhold)
